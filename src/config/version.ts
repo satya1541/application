9050c0e845d43294a2e7adf0b009d6b90c6bb436
Satyanarayana Behera <satyastudy24@gmail.com>
2026-09-22 15:55:19 +0530

    chore(release): bump version to 4.0.6 for artist discography update

--- src/config/version.ts
+++ src/config/version.ts
@@ -4,10 +4,10 @@
  */
 export const APP_CONFIG = {
   name: 'Shorty',
-  baseVersion: '4.0',
-  otaVersion: '4.0.5',
-  releaseTag: 'v4.0.5',
-  releaseName: 'Playlist Live Search Fix & YouTube InnerTube API',
-  releaseDate: '2026-09-21',
+  baseVersion: '4.0.6',
+  otaVersion: '4.0.6',
+  releaseTag: 'v4.0.6',
+  releaseName: 'Artist Profile Discography & Album/Single Playback',
+  releaseDate: '2026-09-22',
   channel: 'preview',
 };
